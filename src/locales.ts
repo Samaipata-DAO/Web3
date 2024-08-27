a4561e0155a2d83f0a997e75f8d42f165eac50b1
Pernest87 <samaipata@skiff.com>
2024-08-27 23:57:01 +0200

    Primera

---
+++ src/locales.ts
@@ -0,0 +1,29 @@
+// locale settings for this theme
+// https://astro-i18n-starter.pages.dev/setup/
+
+export const DEFAULT_LOCALE_SETTING: string = "en";
+
+export const LOCALES_SETTING: LocaleSetting = {
+  "en": {
+    "label": "English"
+  },
+  "es": {
+    "label": "Español"
+  },
+  "fr": {
+    "label": "Français",
+    
+  },
+  "pt": {
+    "label": "Português",
+    
+  },
+};
+
+interface LocaleSetting {
+  [key: Lowercase<string>]: {
+    label: string;
+    lang?: string;
+    dir?: 'rtl' | 'ltr';
+  };
+} // refer: https://starlight.astro.build/reference/configuration/#locales
